5e0f2373ce057403316c08dd679b3609a8abcf24
Chance Smith <chancesmith97@hotmail.com>
2020-07-21 23:51:58 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,9 @@ ActiveRecord::Schema.define(version: 1) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "school_classes", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "room_number"
+  end
+
 end
